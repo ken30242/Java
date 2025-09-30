685074b879033a38fea69ec03b7268f7d193ce4e
Kawin Samphaojan <ken10592@gmail.com>
2025-09-30 13:42:54 +0700

    Add files via upload

---
+++ Basic Java/Variable.java
@@ -0,0 +1,11 @@
+    public class Variable {
+    public static void main(String[] args) {
+        int a = 2;// ประกาศตัวเเปลว่า a ค่าเริ่มต้นเป็น 2
+        double b = 3.4; //ประกาศตัวเเปลว่า b ค่าเริ่มต้นเป็น3.4 ใช้ double เพราะเป็นเลขทศนิยม 
+     //   double c = a+b; ถ้าต้องการผลรวมก็ ปริ้น "" +c
+        System.out.println(" price "+a);
+        System.out.println(" price "+b);
+        }
+    }
+
+    
