bdadf618a6b144d12e6098823cc5931f45e3e578
Kawin Samphaojan <ken10592@gmail.com>
2025-09-30 13:40:47 +0700

    Add files via upload

---
+++ Basic Java/forloop.java
@@ -0,0 +1,8 @@
+public class forloop{
+public static void main(String[] args) {
+    
+    for(int i = 10; i<= 50; i+=10)//ประกาศตัวเเปร i เป็น2 เเละถ้าiน้อยกว่าหรือเท่ากับ10 ให้เพิ่มขึ้นทีละ2
+System.out.println("i"+    i);
+}
+
+}
